1eb22612a0d571006688e2e73dfd74c6d7288528
Alina Martynenko <martynenko.av.94@mail.ru>
2023-12-25 13:27:03 +1000

    deploy

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from '../types';
+
+export const defaultUser: LocalGithubUser = {
+  login: 'martynenkoAl',
+  avatar: 'https://avatars.githubusercontent.com/u/121212086?v=4',
+  name: 'Alina Martynenko',
+  company: '',
+  blog: '',
+  location: 'Vladivostok',
+  bio: 'Frontend Developer',
+  twitter: '',
+  repos: 20,
+  followers: 0,
+  following: 0,
+  created: '2022-12-22T04:13:24Z',
+};
